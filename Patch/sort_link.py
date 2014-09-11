c68e481f3189bc6f3026e1b27c1a86241d66811d
beibeihome <beibeihome88@gmail.com>
2014-09-11 19:04:35 +0800

    previous test 3

--- Patch/sort_link.py
+++ Patch/sort_link.py
@@ -8,8 +8,8 @@ db = MongoClient()[CONSTANT.DATABASE]
 
 for link in db.link.find():
     db.link.update({'_id': link['_id']}, {'$set': {'TYPE': node_link_type[link['TYPE1']]}})
-    print 'link[\'TYPE\']: ' + link['TYPE1']
-    print 'node_link_type[link[\'TYPE\']]: '
+    print 'link[\'TYPE1\']: ' + link['TYPE1']
+    print 'node_link_type[link[\'TYPE1\']]: ' + node_link_type[link['TYPE1']]
     if link['TYPE'] == 'Tiaokong':
         db.link.update({'_id': link['_id']}, {'$set': {'TYPE': link['Function']}})
 
