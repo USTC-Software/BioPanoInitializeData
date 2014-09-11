cb391f215bb4ff76e7de10aec684775af7630607
beibeihome <beibeihome88@gmail.com>
2014-09-11 19:00:45 +0800

    adding type of link test 2

--- Patch/sort_link.py
+++ Patch/sort_link.py
@@ -3,11 +3,13 @@ __author__ = 'Beibeihome'
 from pymongo import *
 import CONSTANT
 
-node_link_type = {'Reaction': 'React', 'Compound': 'React', 'Enzyme': 'Tiaokong', 'sRNA': 'Tiaokong', 'Gene': 'Product', 'TF': 'Tiaokong'}
+node_link_type = {'Reaction': 'React', 'Compound': 'React', 'Enzyme': 'Catalyze', 'sRNA': 'Tiaokong', 'Gene': 'Produce', 'TF': 'Tiaokong'}
 db = MongoClient()[CONSTANT.DATABASE]
 
 for link in db.link.find():
     db.link.update({'_id': link['_id']}, {'$set': {'TYPE': node_link_type[link['TYPE1']]}})
+    print 'link[\'TYPE\']: ' + link['TYPE1']
+    print 'node_link_type[link[\'TYPE\']]: '
     if link['TYPE'] == 'Tiaokong':
         db.link.update({'_id': link['_id']}, {'$set': {'TYPE': link['Function']}})
 
