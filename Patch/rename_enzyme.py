8d299aac8dab0a0b2b708a4dd0dd2b65de481473
beibeihome <beibeihome88@gmail.com>
2014-09-11 22:26:24 +0800

    ['node'] store Id but not Object,fix that

--- Patch/rename_enzyme.py
+++ Patch/rename_enzyme.py
@@ -67,7 +67,9 @@ def separate(enzyme):
                 node = db.node.find_one({'_id': another_node['node']})
                 setLink(new_node, node, 'Enzyme', 'Reaction')
             elif another_node['direct'] == 0:
-                match_flat = new_node['NAME'] == db.uniprot.find_one({'gene_name': base_name_to_uni(another_node['node']['NAME'])})['protein_name']
+                another_name = db.node.find_one({'_id': another_node['node']})['NAME']
+                protein_name = db.uniprot.find_one({'gene_name': base_name_to_uni(another_name)})['protein_name']
+                match_flat = (new_node['NAME'] == protein_name)
                 if match_flat is False:
                     continue
                 node = db.node.find_one({'_id': another_node['node']})
