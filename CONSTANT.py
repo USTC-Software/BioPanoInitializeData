4f471dcdd591de4c35c519fad5a0ea0ae88a58d6
Yifan Gao <git@gaoyifan.com>
2014-10-23 23:30:07 +0800

    fix database connection

--- CONSTANT.py
+++ CONSTANT.py
@@ -6,6 +6,6 @@ from pymongo.mongo_replica_set_client import MongoReplicaSetClient
 #DATABASE = 'igemdata_new'
 DATABASE = 'dump_new'
 
-client = MongoReplicaSetClient('mongodb://import:Dmd2WkjlpmBfInLTY20swgsGO2CQF0bHXn3mWS0niLsJNq0ZqEiiSzNZv0YRUk@us-ce-0:27017,cn-ah-0:27017,cn-bj-0:27017', replicaSet='replset')
+client = MongoReplicaSetClient('mongodb://import:Dmd2WkjlpmBfInLTY20swgsGO2CQF0bHXn3mWS0niLsJNq0ZqEiiSzNZv0YRUk@us-ce-0:27017,cn-ah-0:27017,cn-bj-0:27017/igemdata_new', replicaSet='replset')
 db = client[DATABASE]
 LOG_PATH = './log/log' + str(datetime.now().month) + '_' + str(datetime.now().day) + '.txt'
